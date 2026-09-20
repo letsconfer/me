1202cbb86e9234e228f16e43dead5804c7b16f49
Vikramjit Singh <111748657+letsconfer@users.noreply.github.com>
2026-09-20 16:38:41 +0530

    Update master-config.js

--- master-config.js
+++ master-config.js
@@ -3,12 +3,12 @@
 // ==========================================
 window.CV_MASTER_CONFIG = {
     // 1. Security & Interaction Controls
-    allowRightClick: true,     // false = Restricted (Default), true = Allowed
-    allowLeftClick: true,       // true = Normal clicks, false = Blocks selection/clicks
+    allowRightClick: false,     // false = Restricted (Default), true = Allowed
+    allowLeftClick: false,       // true = Normal clicks, false = Blocks selection/clicks
     allowScreenshot: false,     // false = Blocks PrintScreen/shortcuts, true = Allowed
     
     // 2. Toolbar Icon Controls
-    showDownloadIcon: false,     // true = Show PDF download icon, false = Hidden
+    showDownloadIcon: true,     // true = Show PDF download icon, false = Hidden
     showThemeIcon: false,        // true = Show Theme toggle icon, false = Hidden
     
     // 3. Profile Image Setting (Filename or uploaded image path)
